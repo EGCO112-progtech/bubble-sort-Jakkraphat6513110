ba4a45e3d299f2d8d3171923e4f5adc477a0a7c1
Jakkraphat6513110 <123150808+Jakkraphat6513110@users.noreply.github.com>
2023-01-27 17:06:20 +0700

    change

--- main.c
+++ main.c
@@ -1,19 +1,41 @@
-#define N 6
+// #define N 6
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "sorting.h"
 
-int main() {
-  int a[N]={3,2,6,7,3,1};
-  int i,j,new_number;
+int main(int argc, char* argv[]) {
+  // int a[N]={3,2,6,7,3,1};
+  // int a[argc - 1] = argv
+  int i,j = 0,new_number, N, *a;
 
-   display(a,N);
-  bubbleSort(a,N); 
-   //insertion(a,N);
+  N = argc - 1;
+  a = (int*)malloc(sizeof(int) * N);
+  int* prime = (int*)malloc(sizeof(int) * N);
+  for (i = 0; i < N; i++)
+  {
+    a[i] = atoi(argv[i+1]);
+    if (Isprime(a[i]) == 1){
+      prime[j] = a[i];
+      j++;
+    }
+  }
+  N = j;
+  if (N == 0) printf("no prime");
+  else{
+    display(prime,N);
+    bubbleSort(prime,N); 
+    display(prime,N);
+  }
+  // display(a,N);
+  // bubbleSort(a,N); 
+
+  // insertion(a,N);
 
   // selectionSort(a,N);
-   display(a,N);
+
  return 0;
 }
+
+
 
 
--- sorting.h
+++ sorting.h
@@ -1,4 +1,8 @@
 // selection sort function module in C
+#include <math.h>
+
+
+void swap(int *a,int *b);
 
 void display(int a[],int n){
 
@@ -13,16 +17,34 @@ void display(int a[],int n){
 void selectionSort(int data[], int length) 
 { 
 	int i, j, m, mi; 
-	
-     display(data,length);
-     
+	for (i = 0; i < length - 1; i++)
+  {
+    mi = i;
+    for (j = i + 1; j < length; j++)
+    {
+      if (data[j] < data[mi]) mi = j;
+      
+      if (mi != i) swap(&data[i], &data[mi]);
+
+    }
+  display(data,length);
+  }
 	
 } 
 
 
 
 void insertion(int a[],int n){
-
+  for (int i = 0; i < n; i++)
+  {
+    int j = i;
+    while (j > 0 && a[j-1] > a[j])
+    {
+      swap(&a[j-1], &a[j]);
+      j -= 1;
+    }
+    display(a, n);
+  }
 
 }
 
@@ -36,16 +58,32 @@ void swap(int *a,int *b){
 
 void bubbleSort(int a[],int n){
 
-int i,j;
-int sorted;
-// how may pair to compare?
-for(j=1;j<=n-1;j++){
- 
-  display(a,n);
- }
-  
+  // how may pair to compare?
+  for(int i = 0; i < n-1; i++)
+  {
+    int sorted;
+    printf("round %d\n", i);
+    for (int j = 0; j < n-i-1; j++)
+    {
+      if (a[j] > a[j + 1])
+      {
+        swap(&a[j], &a[j+1]);
+        sorted = 1;
+      }
+      display(a, n);
+    }
+    if (sorted == 0) break;
+  }
 }
 
-
+int Isprime(int x) 
+{
+  if (x == 1 || x == 0) return 0;
+  for(int i = 2; i < x; i++)
+  {
+    if (x % i == 0) return 0;
+  }
+  return 1;
+}
 
 
