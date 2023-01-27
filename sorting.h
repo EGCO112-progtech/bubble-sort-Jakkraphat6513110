a59c3a03d1eeefca72d2d80ff11168d1e558f9e0
Jakkraphat6513110 <123150808+Jakkraphat6513110@users.noreply.github.com>
2023-01-27 17:22:37 +0700

    Update sorting.h

--- sorting.h
+++ sorting.h
@@ -62,7 +62,6 @@ void bubbleSort(int a[],int n){
   for(int i = 0; i < n-1; i++)
   {
     int sorted;
-    printf("round %d\n", i);
     for (int j = 0; j < n-i-1; j++)
     {
       if (a[j] > a[j + 1])
